45c1812f95d962682d3153c38e88617ce8a301a4
Ubuntu <ubuntu@ip-172-31-35-106.us-west-2.compute.internal>
2013-07-05 03:52:00 +0000

    change to web.js

--- web.js
+++ web.js
@@ -1,11 +1,18 @@
-var express = require('express');
+var buf = new Buffer('ABCDEF');
+console.log(buf.length);     // 6
+console.log(buf.toString());var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
 fs.readFile('index.html',function (err, data) {
  // if (err) throw err;
-console.log(data); 
- mainData = data
+//console.log(data); 
+// mainData = data
+
+buf = new Buffer(data);
+console.log(buf.length);     // 6
+console.log(buf.toString()); // ABCDEF
+mainData = buf.toString();
  // return mainData;
 });
 
