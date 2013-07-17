e947141bf7b481d8b4971219530a4a6c054b389c
Ubuntu <ubuntu@ip-172-31-35-106.us-west-2.compute.internal>
2013-07-17 03:07:20 +0000

    updated index.html with copy and images

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var buf = new Buffer('ABCDEF');
-console.log(buf.length);     // 6
-console.log(buf.toString());var express = require('express');
+//var buf = new Buffer('ABCDEF');
+//console.log(buf.length);     // 6
+//console.log(buf.toString());
+var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var htmlfile = "index.html";
+/*
 fs.readFile('index.html',function (err, data) {
  // if (err) throw err;
 //console.log(data); 
@@ -15,9 +17,10 @@ buf = new Buffer(data);
 mainData = buf.toString();
  // return mainData;
 });
-
+*/
 app.get('/', function(request, response) {
-  response.send(mainData);
+ var html = fs.readFileSync(htmlfile).toString();
+ response.send(html);
 });
 
 var port = process.env.PORT || 8080;
